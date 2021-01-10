9eb93be476d226c848eba0eeae4b6da965aac670
JannatJameel <j_annat@outlook.com>
2021-01-10 13:15:47 +0300

    Unpublished story

---
+++ story.js
@@ -0,0 +1,5 @@
+const name = "Jannat";
+const age = 23;
+const hobby = "food loving";
+
+console.log (`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
